2fc84549b85781570e3e31bfd547685c108f9e48
DanielLu127 <lxinwei7@vt.edu>
2022-10-03 14:36:38 -0700

    init project

---
+++ src/main/java/com/cloud/cloudForum/CloudForumApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudForumApplication.class, args);
+	}
+
+}
