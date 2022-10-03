e2e76f3c9439cdbc207dfc2dbef14cdc496aa2af
DanielLu127 <lxinwei7@vt.edu>
2022-10-03 14:56:49 -0700

    change port to 8081 add logger in main method

--- src/main/java/com/cloud/cloudForum/CloudForumApplication.java
+++ src/main/java/com/cloud/cloudForum/CloudForumApplication.java
@@ -1,13 +1,20 @@
 package com.cloud.cloudForum;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CloudForumApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(CloudForumApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(CloudForumApplication.class, args);
+		Environment env  = SpringApplication.run(CloudForumApplication.class, args).getEnvironment();
+		LOG.info("Successfully start");
+		LOG.info("start at port:{}", env.getProperty("server.port"));
 	}
 
 }
